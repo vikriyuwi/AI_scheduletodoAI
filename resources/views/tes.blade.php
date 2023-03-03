994dd73a1822f8b29454ba71e9476c8bd6ab76d8
badutHutan <vinchen996@gmail.com>
2023-03-03 23:31:15 +0700

    update view tes

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -19,10 +19,9 @@
             <h1>
                 Add new list:
             </h1>
-            @if (!isset($submitted))
             <div class="card" id="taskCard">
                 <div class="card-body">
-                    <form method="POST" action="" class="task">
+                    <form action="submit">
                         @csrf
                         <label for="taskName">Task Name:</label>
                         <input type="text" id="taskName" name="taskName" required><br><br>
@@ -36,30 +35,26 @@
                         <label for="taskSteps">Task Steps:</label>
                         <input type="number" id="taskSteps" name="taskSteps" min="1" required><br><br>
 
-                        <button type="submit">Submit</button>
+                        <button type="submit" id="submit-button">Submit</button>
                     </form>
                 </div>
             </div>
-            @else
-            <div class="card" id="stepCard">
+            <div class="card" id="stepCard" style="display: none">
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form action="submit">
                         @csrf
-                        <input type="hidden" id="taskName" name="taskName" value="{{ $taskName }}">
-                        <input type="hidden" id="taskDeadline" name="taskDeadline" value="{{ $taskDeadline }}">
-                        <input type="hidden" id="taskDifficulty" name="taskDifficulty" value="{{ $taskDifficulty }}">
-                        <input type="hidden" id="taskSteps" name="taskSteps" value="{{ $taskSteps }}">
+                        <input type="hidden" id="taskName" name="taskName">
+                        <input type="hidden" id="taskDeadline" name="taskDeadline">
+                        <input type="hidden" id="taskDifficulty" name="taskDifficulty">
+                        <input type="hidden" id="taskSteps" name="taskSteps">
 
-                        @for ($i = 1; $i <= $taskSteps; $i++)
+                        {{-- @for ($i = 1; $i <= $taskSteps; $i++)
                         <label for="stepName{{ $i }}">Step {{ $i }}:</label>
                         <input type="text" id="stepName{{ $i }}" name="stepName{{ $i }}" required><br><br>
-                        @endfor
-
-                        <button type="submit">Submit</button>
+                        @endfor --}}
                     </form>
                 </div>
             </div>
-            @endif
         </div>
     </main>
 
@@ -67,5 +62,38 @@
 
     </footer>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('#submit-button').click(function(event) {
+        event.preventDefault();
+        var taskName = $('#taskName').val();
+        var taskDeadline = $('#taskDeadline').val();
+        var taskDifficulty = $('#taskDifficulty').val();
+        var taskSteps = $('#taskSteps').val();
+        $('#taskCard').hide();
+        var newFormCard = $('#stepCard')
+        var newForm = $('form').eq(1);
+        newFormCard.show();
+        newForm.find('#taskName').val(taskName);
+        newForm.find('#taskDeadline').val(taskDeadline);
+        newForm.find('#taskDifficulty').val(taskDifficulty);
+        newForm.find('#taskSteps').val(taskSteps);
+        for (var i = 1; i <= taskSteps; i++) {
+            var stepName = $('<label>', {
+            for: 'stepName' + i,
+            text: 'Step ' + i + ':'
+            }).add($('<input>', {
+            type: 'text',
+            id: 'stepName' + i,
+            name: 'stepName' + i,
+            required: true
+            }));
+            newForm.append(stepName).append('<br><br>');
+        }
+        newForm.append($('<button type="submit">Submit</button>'))
+        });
+    });
+    </script>
 </body>
 </html>
