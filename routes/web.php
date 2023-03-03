209ebc6d7487afd9465786a2d33a9d3ec00fc341
latifulfikri <latifulfikri1902@gmail.com>
2023-03-03 23:13:30 +0700

    new UserPageController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserPagesController as UPC;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('tes');
+
+
+Route::prefix('/')->group(function() {
+    Route::get('/', function () {
+        return view('tes');
+    });
+    Route::get('/template', function () {
+        return view('template');
+    });
 });
