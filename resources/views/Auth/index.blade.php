59861a6dcd3f74f3c5a2c9eccbb452d4a21a09bf
Fikri Yuwi <latifulfikri1902@gmail.com>
2023-03-16 20:50:32 +0700

    fix front end login

--- resources/views/Auth/index.blade.php
+++ resources/views/Auth/index.blade.php
@@ -3,22 +3,24 @@
 @section('main-content')
 
 <div class="container vh-100">
-    <div class="row vh-100 py-3">
-        <div class="col-md-5 text-md-start text-center my-auto">
+    <div class="row vh-100">
+        <div class="col-md-5 text-md-start text-center my-auto p-5">
             <img src="{{ url('assets/logo/icon.png') }}" alt="logo box" width="96">
             <div class="row mt-4">
-                <div class="mb-2">
+                <div class="col-12 mb-2">
                     <h1>Google login</h1>
-                <p>Your one step closer for managing your tasks and staying on top of your to-do list. 
+                    <p>Your one step closer for managing your tasks and staying on top of your to-do list. 
                     Log in now and start achieving your goals one task at a time.</p>
                 </div>
-                <a href="{{ url('auth/google') }}" class="btn btn-danger rounded-pill text-white mb-2"><i class="fa-brands fa-google"></i> Login with Google</a>
-                <a href="{{url('/')}}" class="btn btn-secondary rounded-pill">Go back to home</a> 
-            </div> 
+                <div class="">
+                    <a href="{{ url('auth/google') }}" class="btn btn-danger rounded-pill text-white mb-2 d-block"><i class="fa-brands fa-google"></i> Login with Google</a>
+                    <a href="{{url('/')}}" class="btn btn-secondary rounded-pill d-block">Go back to home</a> 
+                </div>
+            </div>
         </div>
         <div class="col-md-2 d-none d-md-block"></div>
-        <div class="col-md-5 my-auto d-none d-md-block">
-            <img src="{{ url('assets/element/Component_4.png') }}" alt="pic login" class="img-fluid rounded-4">
+        <div class="col-md-5 my-auto d-none d-md-block p-5 vh-100">
+            <img src="{{ url('assets/element/Component_4.png') }}" alt="pic login" class="img-fluid rounded-5" style="height:100%">
         </div>
     </div>
 </div>
